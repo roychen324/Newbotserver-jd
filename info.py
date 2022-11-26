9de701ed9f7110aeb09a85e0f6712e348b0e2de2
Localusersbot <115877356+Localusersbot@users.noreply.github.com>
2022-11-26 11:40:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,8 +47,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/jdbotsupportgroup')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+ip38nA3Tm0A3MGRl')
-MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+80jyV1Ci7S4wM2U1')
+MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?'')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'jdbotsupportgroup')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
